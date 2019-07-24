bbc4f49e1cbc3c8686787c492acd7e10e6736a89
Huy1411 <53205463+Huy1411@users.noreply.github.com>
2019-07-24 18:27:26 +0700

    Bai 1 - Bai 2

--- BaiTap24_07_2019/Bai_2/main.c
+++ BaiTap24_07_2019/Bai_2/main.c
@@ -1,21 +1,58 @@
 #include <stdio.h>
 
 int main() {
-    int a, b;
-    printf("So thu nhat: dsfasdf= ");
+    int a, b,c;
+    printf("So nguyen thu nhat: a = ");
     scanf("%d", &a);
-    printf("So thu 2: b= ");
+    printf("So nguyen thu 2: b = ");
     scanf("%d", &b);
+    printf("So nguyen thu 3: c =");
+    scanf("%d",&c);
 
+    int max=a,min=a;
 
-    if (a > b) {
-        if (b != 0) {
-            printf("%d/%d=%d", a, b, a / b);
-        } else {
-            printf("khong co ket qua");
-        }
-    } else {
-        printf("%d*%d=%d", a, b, a * b);
+    if(max<b){
+        max = b;
     }
+    if(max<c){
+        max = c;
+    }
+
+    if(min>b){
+        min = b;
+    }
+    if(min>c){
+        min = c;
+    }
+
+    printf("So lon nhat la: %d\n",max);
+    printf("So nho nhat la: %d\n",min);
+
+
+//    if (a>b) ;{
+//        if (b>c) {
+//            printf("So nho nhat la: %d\n", c);
+//            printf("So lon nhat la: %d\n",a);
+//        } else {
+//            if(a>c){
+//                printf("So lon nhat la: %d\n",a);
+//            } else{
+//                printf("So lon nhat la: %d\n",c);
+//            }
+//            printf("So nho nhat la: %d\n",b);
+//        }
+//    } else {
+//        if (a>c) {
+//            printf("So nho nhat la: %d\n", c);
+//            printf("So lon nhat la: %d\n",b);
+//        } else {
+//            if(b>c){
+//                printf("So lon nhat la: %d\n",b);
+//            } else{
+//                printf("So lon nhat la: %d\n",c);
+//            }
+//            printf("So nho nhat la: %d\n",a);
+//        }
+//    }
     return 0;
 }
